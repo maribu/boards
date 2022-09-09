30ae36d2f4d9701fac620be1e40878a979758492
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2022-09-09 15:53:10 +0200

    riot/m1284p: update to current master & SD card conf

--- riot/m1284p/include/board.h
+++ riot/m1284p/include/board.h
@@ -25,6 +25,7 @@
 
 #include "cpu.h"
 #include "periph_conf.h"
+#include "mtd.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -38,33 +39,44 @@ extern "C" {
 /** @} */
 
 /**
- * @name    xtimer configuration values
- * @{
- */
-#define XTIMER_WIDTH                (16)
-#define XTIMER_HZ                   (CLOCK_CORECLOCK / 64)
-#define XTIMER_BACKOFF              (80)
-#define XTIMER_ISR_BACKOFF          (120)
-/** @} */
-
-/**
  * @name    ztimer configuration values
  * @{
  */
-#define CONFIG_ZTIMER_USEC_TYPE     ZTIMER_TYPE_PERIPH_TIMER
-#define CONFIG_ZTIMER_USEC_DEV      (TIMER_DEV(0))
-#define CONFIG_ZTIMER_USEC_FREQ     XTIMER_HZ
-#define CONFIG_ZTIMER_USEC_WIDTH    (16)
+#define CONFIG_ZTIMER_USEC_TYPE         ZTIMER_TYPE_PERIPH_TIMER
+#define CONFIG_ZTIMER_USEC_DEV          (TIMER_DEV(0))
+#define CONFIG_ZTIMER_USEC_BASE_FREQ    (CLOCK_CORECLOCK / 64)
+#define CONFIG_ZTIMER_USEC_WIDTH        (16)
 /** @} */
 
 /**
- * @name    STDIO configuration
+ * @name    CC110x configuration
  * @{
  */
-#define CC110X_PARAM_CS             GPIO_PIN(PORT_B, 0)
-#define CC110X_PARAM_GDO0           GPIO_PIN(PORT_B, 2)
-#define CC110X_PARAM_GDO2           GPIO_PIN(PORT_D, 3)
+#define CC110X_PARAM_CS                 GPIO_PIN(PORT_B, 0)
+#define CC110X_PARAM_GDO0               GPIO_PIN(PORT_B, 2)
+#define CC110X_PARAM_GDO2               GPIO_PIN(PORT_D, 3)
 /** @} */
+
+/**
+ * @name micro-sd slot pin definitions
+ * @{
+ */
+#define SDCARD_SPI_PARAM_SPI            SPI_DEV(0)          /**< SPI device */
+#ifndef SDCARD_SPI_PARAM_CS
+#define SDCARD_SPI_PARAM_CS             GPIO_PIN(PORT_B, 0) /**< Chip Select */
+#endif
+#define SDCARD_SPI_PARAM_CLK            GPIO_PIN(PORT_B, 7) /**< Serial Clock */
+#define SDCARD_SPI_PARAM_MOSI           GPIO_PIN(PORT_B, 5) /**< Master Output, Slave Input */
+#define SDCARD_SPI_PARAM_MISO           GPIO_PIN(PORT_B, 6) /**< Master Input, Slave Output */
+#define SDCARD_SPI_PARAM_POWER          GPIO_UNDEF          /**< Powen on/off */
+#define CARD_DETECT_PIN                 GPIO_UNDEF          /**< Pin for card detect */
+/** @} */
+
+/** Default MTD device */
+#define MTD_0 mtd0
+
+/** mtd flash emulation device */
+extern mtd_dev_t *mtd0;
 
 /**
  * @brief   Initialize board specific hardware, including clock, LEDs and std-IO
